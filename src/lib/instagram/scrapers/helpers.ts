1cbea984217cba8026ea68695813e8f93ebe6546
dhruvermafz <vermadhruv09112002@gmail.com>
2023-12-26 12:06:24 +0530

    error fixed

--- src/lib/instagram/scrapers/helpers.ts
+++ src/lib/instagram/scrapers/helpers.ts
@@ -1,15 +1,14 @@
 import { BadRequest, TimeoutException } from "@/lib/exceptions";
 import { getTimedFilename } from "@/utils";
 
-export const getIGVideoFileName = () => {
-    getTimedFilename("ig-downloader", "mp4");
-}
+export const getIGVideoFileName = () =>
+  getTimedFilename("ig-downloader", "mp4");
 
 export const handleScraperError = (error: any) => {
-    console.log("Scrapper error:", error.message);
-    if(error.message.includes("Status Code 404")) {
-        throw new BadRequest("This post is private or does not exist", 404);
-    } else if(error instanceof TimeoutException) {
-        throw new TimeoutException();
-    }
-}
+  console.log("Scraper error:", error.message);
+  if (error.message.includes("status code 404")) {
+    throw new BadRequest("This post is private or does not exist", 404);
+  } else if (error instanceof TimeoutException) {
+    throw new TimeoutException();
+  }
+};
